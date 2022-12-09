840a1f8f7c67e493ef6e3c2f91b9006465e64abb
Alexander Smith <37078247+AlexanderHughSmith@users.noreply.github.com>
2022-12-09 15:11:05 -0500

    Updating color colors

--- app/src/main/java/com/example/hatman/ui/screens/about/AboutContent.kt
+++ app/src/main/java/com/example/hatman/ui/screens/about/AboutContent.kt
@@ -1,25 +1,40 @@
 package com.example.hatman.ui.screens.about
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Build
+import android.provider.ContactsContract.CommonDataKinds.Email
 import android.widget.RadioGroup
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.hatman.BuildConfig
+import com.example.hatman.R
 import com.example.hatman.ui.SharedViewModel
 import com.example.hatman.ui.theme.LARGE_PADDING
 import com.google.android.material.button.MaterialButtonToggleGroup
+import java.security.AccessController.getContext
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,10 +52,13 @@ fun AboutContent(
             BasicTextRow("Version: ${BuildConfig.VERSION_NAME}")
         }
         item {
-            BasicTextRow("Feedback Email: HatmanFeedback@protonmail.com")
-        }
-        item {
-            BasicTextRow("App created by: Alexander Smith")
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text("Feedback Email:")
+                EmailAddress("HatmanFeedback@protonmail.com", LocalContext.current)
+            }
         }
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             item {
@@ -51,7 +69,7 @@ fun AboutContent(
                     horizontalArrangement = Arrangement.Center,
                     verticalAlignment = Alignment.CenterVertically
                 ){
-                    Text("Dynamic colors ")
+                    Text("Use dynamic colors ")
                     Checkbox(checked = sharedViewModel.useDynamicColors.value, onCheckedChange = {
                         sharedViewModel.useDynamicColors.value = it
                         sharedViewModel.updateDynamicColors()
@@ -153,6 +171,45 @@ fun AboutContent(
             }
         }
     }
+}
+
+@Composable
+fun EmailAddress(email: String, context: Context) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ){
+        Text(
+            modifier = Modifier.weight(5f),
+            text = email,
+            textAlign = TextAlign.Center
+        )
+        IconButton(
+            modifier = Modifier.weight(1f),
+            onClick = {
+                // Copy the email address to the clipboard
+                copyToClipboard(email, context)
+            },
+        ){
+            Icon(
+                painter = painterResource(id = R.drawable.copy),
+                contentDescription = "Copy email address to clipboard"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EmailAddressPreview() {
+    EmailAddress("HatmanFeedback@protonmail.com", LocalContext.current)
+}
+
+fun copyToClipboard(text: String, context: Context) {
+    val clipboard = ContextCompat.getSystemService(context,ClipboardManager::class.java)
+    clipboard?.setPrimaryClip(ClipData.newPlainText("",text))
 }
 
 @Composable
--- app/src/main/java/com/example/hatman/ui/screens/options/OptionsScreenAppBar.kt
+++ app/src/main/java/com/example/hatman/ui/screens/options/OptionsScreenAppBar.kt
@@ -45,6 +45,9 @@ fun SetupScreenAppBar(
                 )
             }
         },
+        /*colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+        ),*/
     )
 }
 
